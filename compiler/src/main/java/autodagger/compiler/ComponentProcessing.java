9021d88babfdfe633e21999667d8ec0eb7a4e9cf
Sergey Chuvashev <chuvashev@mqd.me>
2016-03-22 18:14:04 +0400

    Superinterfaces may be autocomponent target classes

--- compiler/src/main/java/autodagger/compiler/ComponentProcessing.java
+++ compiler/src/main/java/autodagger/compiler/ComponentProcessing.java
@@ -1,23 +1,16 @@
 package autodagger.compiler;
 
+import autodagger.AutoComponent;
+import autodagger.AutoSubcomponent;
+import autodagger.compiler.utils.AutoComponentClassNameUtil;
 import com.google.auto.common.MoreElements;
 import com.google.auto.common.MoreTypes;
 import com.google.common.collect.ImmutableSet;
-import com.squareup.javapoet.AnnotationSpec;
-import com.squareup.javapoet.ClassName;
-import com.squareup.javapoet.MethodSpec;
-import com.squareup.javapoet.ParameterSpec;
-import com.squareup.javapoet.ParameterizedTypeName;
-import com.squareup.javapoet.TypeName;
-
-import org.apache.commons.lang3.StringUtils;
-
-import java.lang.annotation.Annotation;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import com.squareup.javapoet.*;
+import processorworkflow.AbstractComposer;
+import processorworkflow.AbstractProcessing;
+import processorworkflow.Errors;
+import processorworkflow.ProcessingBuilder;
 
 import javax.lang.model.element.Element;
 import javax.lang.model.element.ElementKind;
@@ -25,15 +18,8 @@ import javax.lang.model.element.Modifier;
 import javax.lang.model.type.TypeMirror;
 import javax.lang.model.util.Elements;
 import javax.lang.model.util.Types;
-
-import autodagger.AutoComponent;
-import autodagger.AutoSubcomponent;
-import autodagger.compiler.utils.AutoComponentClassNameUtil;
-import processorworkflow.AbstractComposer;
-import processorworkflow.AbstractProcessing;
-import processorworkflow.Errors;
-import processorworkflow.Logger;
-import processorworkflow.ProcessingBuilder;
+import java.lang.annotation.Annotation;
+import java.util.*;
 
 /**
  * @author Lukasz Piliszczuk - lukasz.pili@gmail.com
@@ -141,7 +127,7 @@ public class ComponentProcessing extends AbstractProcessing<ComponentSpec, State
             componentSpec.setDependenciesTypeNames(getDependencies());
 
             // superinterfaces
-            componentSpec.setSuperinterfacesTypeNames(ProcessingUtil.getTypeNames(extractor.getSuperinterfacesTypeMirrors()));
+            componentSpec.setSuperinterfacesTypeNames(getSuperinterfaces(extractor.getSuperinterfacesTypeMirrors()));
 
             // modules
             componentSpec.setModulesTypeNames(ProcessingUtil.getTypeNames(extractor.getModulesTypeMirrors()));
@@ -201,6 +187,34 @@ public class ComponentProcessing extends AbstractProcessing<ComponentSpec, State
 
             mainLoop:
             for (TypeMirror typeMirror : extractor.getDependenciesTypeMirrors()) {
+                // check if dependency type mirror references an @AutoComponent target
+                // if so, build the TypeName that matches the target component
+                for (ComponentExtractor componentExtractor : extractors) {
+                    if (componentExtractor == extractor) {
+                        // ignore self
+                        continue;
+                    }
+
+                    if (ProcessingUtil.areTypesEqual(componentExtractor.getTargetTypeMirror(), typeMirror)) {
+                        typeNames.add(AutoComponentClassNameUtil.getComponentClassName(componentExtractor.getComponentElement()));
+                        continue mainLoop;
+                    }
+                }
+
+                typeNames.add(TypeName.get(typeMirror));
+            }
+
+            return typeNames;
+        }
+
+        private List<TypeName> getSuperinterfaces(List<TypeMirror> superinterfaces) {
+            List<TypeName> typeNames = new ArrayList<>();
+            if (superinterfaces == null) {
+                return typeNames;
+            }
+
+            mainLoop:
+            for (TypeMirror typeMirror : superinterfaces) {
                 // check if dependency type mirror references an @AutoComponent target
                 // if so, build the TypeName that matches the target component
                 for (ComponentExtractor componentExtractor : extractors) {
